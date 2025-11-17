f2501da9755acb13914edb597ba2296027dd7c07
ADevlooper <syedasarudeen.s@gmail.com>
2025-11-17 16:02:01 +0530

    minor changes made

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -14,12 +14,12 @@ const router = express.Router();
 router.get("/", getAllProducts);
 router.get("/new", (req, res) => res.render("new"));
 
-// IMPORTANT: match front-end field name!
+
 router.post("/create", upload.single("image"), createProduct);
 
 router.get("/edit/:id", getProductById);
 
-// IMPORTANT: match front-end field name!
+
 router.post("/update/:id", upload.single("image"), updateProduct);
 
 router.post("/delete/:id", deleteProduct);
--- server.js
+++ server.js
@@ -8,21 +8,21 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Body parsers
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve static files (CSS, JS)
+
 app.use(express.static(path.join(__dirname, "public")));
 
-// Serve uploaded images publicly
+
 app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
 
-// EJS View Engine
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Routes
+
 app.use("/", productsRouter);
 
 app.listen(3000, () =>
